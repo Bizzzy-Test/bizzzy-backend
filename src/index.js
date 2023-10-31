acfcf3bca5f27f7aa991df3622e1cc297370fd95
Rohit Sharma <rohit.sharma@spineor.com>
2023-10-31 22:20:40 +0530

    update apis routes

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api', routes);
+app.use('/api/v1', routes);
 
 app.use('/', routesWithoutAPI);
 // Comment Socket
