b0f7ee89c94de206e072d4425dff349778118b30
professionaltechs <professionalstechs@gmail.com>
2023-11-04 01:16:46 +0500

    changes

--- src/routes/jobProposal/index.js
+++ src/routes/jobProposal/index.js
@@ -1,8 +1,9 @@
 const jobProposalAPI = require("../../controller/jobProposal");
+const { jsonWebToken } = require("../../middleware");
 const { urlConstants } = require("../../constants");
 
 module.exports = (app) => {
-    app.post(urlConstants.CREATE_JOB_PROPOSAL, jobProposalAPI.createJobProposal);
-    app.get(urlConstants.GET_JOB_PROPOSALS_BY_JOB_ID, jobProposalAPI.getJobProposalByJobId);
-    app.get(urlConstants.GET_JOB_PROPOSALS_BY_USER_ID, jobProposalAPI.getJobProposalByUserId);
+    app.post(urlConstants.CREATE_JOB_PROPOSAL, jsonWebToken.validateToken, jobProposalAPI.createJobProposal);
+    app.get(urlConstants.GET_JOB_PROPOSALS_BY_JOB_ID, jsonWebToken.validateToken, jobProposalAPI.getJobProposalByJobId);
+    app.get(urlConstants.GET_JOB_PROPOSALS_BY_USER_ID, jsonWebToken.validateToken, jobProposalAPI.getJobProposalByUserId);
 }
