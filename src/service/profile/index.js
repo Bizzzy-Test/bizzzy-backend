130f768a58c97917e285812798cb7f8088912a68
Harshad Patoliya <harshadpatoliya14@gmail.com>
2023-11-09 12:45:24 +0530

    skills and category field issue

--- src/service/profile/index.js
+++ src/service/profile/index.js
@@ -88,8 +88,8 @@ const freelencerProfile = async (req, userData, res) => {
                 });
             }
         }
-        profile.skills = req.body?.skills ? req.body?.skills : (profile.skills.length !== 0 ? profile.skills : []);
-        profile.categories = req.body?.categories ? req.body?.categories : (profile.categories.length !== 'null' ? profile.categories : []);
+        profile.skills = req.body && req.body.skills !== undefined ? req.body.skills : profile.skills;
+        profile.categories = req.body && req.body.categories !== undefined ? req.body.categories : profile.categories;
         profile.professional_role = req.body?.professional_role ? req.body?.professional_role : (profile.professional_role !== 'null' ? profile.professional_role : 'null');
         profile.title = req.body?.title ? req.body?.title : (profile.title !== 'null' ? profile.title : 'null');
         profile.hourly_rate = req.body?.hourly_rate ? req.body?.hourly_rate : (profile.hourly_rate !== 'null' ? profile.hourly_rate : 'null');
