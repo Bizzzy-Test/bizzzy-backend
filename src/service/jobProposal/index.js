9113e661a29e9e3df4150378bd0742b3f239eefc
Harshad Patoliya <harshadpatoliya14@gmail.com>
2023-11-09 15:04:27 +0530

    Get all proposal by job id query changes

--- src/service/jobProposal/index.js
+++ src/service/jobProposal/index.js
@@ -2,7 +2,7 @@ const { responseData, messageConstants } = require('../../constants');
 const JobProposalSchema = require('../../models/jobProposal');
 const { logger } = require('../../utils');
 
-const createJobProposal  = async (req, res) => {
+const createJobProposal = async (req, res) => {
     return new Promise(async () => {
         const jobProposal = JobProposalSchema(req);
         await jobProposal.save().then(response => {
@@ -16,9 +16,8 @@ const createJobProposal  = async (req, res) => {
 
 const getJobProposalByUserId = async (req, res) => {
     return new Promise(async () => {
-        await JobProposalSchema.find({userId: req.userId}).then(async (result) => {
+        await JobProposalSchema.find({ userId: req.userId }).then(async (result) => {
             return responseData.success(res, result, `job proposal fetched succesfully`);
-            
         }).catch((err) => {
             logger.error(`${messageConstants.INTERNAL_SERVER_ERROR}. ${err}`);
             return responseData.fail(res, `${messageConstants.INTERNAL_SERVER_ERROR}. ${err}`, 500);
@@ -28,9 +27,36 @@ const getJobProposalByUserId = async (req, res) => {
 
 const getJobProposalByJobId = async (req, res) => {
     return new Promise(async () => {
-        await JobProposalSchema.find({jobId: req.jobId}).then(async (result) => {
+        const query = [
+            {
+                $match: { jobId: req.jobId }
+            },
+            {
+                $lookup: {
+                    from: 'users',
+                    let: { userId: { $toObjectId: '$userId' } },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: { $eq: ['$_id', '$$userId'] }
+                            }
+                        },
+                        {
+                            $project: {
+                                _id: 1,
+                                firstName: 1,
+                                lastName: 1,
+                                email: 1,
+                                country: 1
+                            }
+                        }
+                    ],
+                    as: 'user_details'
+                }
+            }
+        ];
+        await JobProposalSchema.aggregate(query).then(async (result) => {
             return responseData.success(res, result, `job proposal fetched succesfully`);
-            
         }).catch((err) => {
             logger.error(`${messageConstants.INTERNAL_SERVER_ERROR}. ${err}`);
             return responseData.fail(res, `${messageConstants.INTERNAL_SERVER_ERROR}. ${err}`, 500);
