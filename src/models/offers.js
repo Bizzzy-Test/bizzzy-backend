bf65d7a74e022df045e165cc91a4ff6a4cd376ed
harshad5122 <harshadpatoliya14@gmail.com>
2024-01-03 17:31:39 +0530

    - send offer API changes

--- src/models/offers.js
+++ src/models/offers.js
@@ -17,16 +17,58 @@ const OfferSchema = mongoose.Schema({
         ref: 'jobs',
         required: true,
     },
-    job_type: {
-        type: String
-    },
     status: {
         type: String,
-        default:'pending'
+        default: 'pending'
     },
     budget: {
         type: Number,
         required: true
+    },
+    hiring_team: {
+        type: String,
+        required: true
+    },
+    job_title: {
+        type: String,
+        required: true
+    },
+    contract_title: {
+        type: String,
+        required: true
+    },
+    job_type: {
+        type: String,
+        enum: ['hourly', 'fixed'],
+        required: true
+    },
+    hourly_rate: {
+        type: Number,
+        required: function () {
+            return this.job_type === 'hourly'
+        },
+    },
+    project_budget: {
+        type: Number,
+        required: function () {
+            return this.job_type === 'fixed'
+        },
+    },
+    weekly_limit: {
+        type: Number,
+        required: function () {
+            return this.job_type === 'hourly'
+        },
+    },
+    allow_freelancer_manually_timelog: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    accept_terms_condition: {
+        type: Boolean,
+        required: true,
+        default: false
     },
     created_at: {
         type: Date,
@@ -36,7 +78,6 @@ const OfferSchema = mongoose.Schema({
         type: Date,
         default: Date.now
     }
-
 });
 
 module.exports = mongoose.model('offers', OfferSchema);
