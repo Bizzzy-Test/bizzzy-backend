9a6aa9f27b3de2d82b96050325ed2924c6fef75e
harshad5122 <harshadpatoliya14@gmail.com>
2024-01-08 18:08:21 +0530

    - deleteMessage API created

--- src/routes/message/index.js
+++ src/routes/message/index.js
@@ -5,4 +5,5 @@ const { jsonWebToken } = require("../../middleware");
 module.exports = (app) => {
     app.get(urlConstants.MESSAGE_LIST, jsonWebToken.validateToken, api.getMessageList);
     app.get(urlConstants.CHAT_USER_LIST, jsonWebToken.validateToken, api.getChatUserList);
+    app.post(urlConstants.DELETE_MESSAGE, jsonWebToken.validateToken, api.deleteMessage);
 }
